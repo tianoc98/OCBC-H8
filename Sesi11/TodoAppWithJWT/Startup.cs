d011ca2d8245c2b37830e5da80c1c3664d1b8f33
tianoc98 <newakmaret2020@gmail.com>
2021-10-18 13:28:06 +0700

    update

--- Sesi11/TodoAppWithJWT/Startup.cs
+++ Sesi11/TodoAppWithJWT/Startup.cs
@@ -34,10 +34,44 @@ namespace TodoAppWithJWT
         {
             
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoAppWithJWT", Version = "v1" });
-            });
+            services.AddSwaggerGen(swagger =>
+                {
+                    swagger
+                        .SwaggerDoc("v1",
+                        new OpenApiInfo {
+                            Title = "TodoApp",
+                            Version = "v1",
+                            Description =
+                                "Authentication and Authorization in ASP.NET 5 with JWT and Swagger"
+                        });
+
+                    // To Enable authorization using Swagger (JWT)
+                    swagger
+                        .AddSecurityDefinition("Bearer",
+                        new OpenApiSecurityScheme()
+                        {
+                            Name = "Authorization",
+                            Type = SecuritySchemeType.ApiKey,
+                            Scheme = "Bearer",
+                            BearerFormat = "JWT",
+                            In = ParameterLocation.Header,
+                            Description =
+                                "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+                        });
+                    swagger
+                        .AddSecurityRequirement(new OpenApiSecurityRequirement {
+                            {
+                                new OpenApiSecurityScheme {
+                                    Reference =
+                                        new OpenApiReference {
+                                            Type = ReferenceType.SecurityScheme,
+                                            Id = "Bearer"
+                                        }
+                                },
+                                new string[] { }
+                            }
+                        });
+                });
             services.AddDbContext<ApiDbContext>(options => 
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")
@@ -92,7 +126,6 @@ namespace TodoAppWithJWT
             }
             
             app.UseAuthentication();
-            app.UseHttpsRedirection();
         
             app.UseRouting();
 
